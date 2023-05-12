9a23d815197463bd7c57daa296d2528f7286e788
Neeraj Anand <75099297+coddington@users.noreply.github.com>
2023-05-12 12:22:28 +0530

    Update db_setup.py

--- db/db_setup.py
+++ db/db_setup.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://gwen@localhost/fast_lms"
+# SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://gwen@localhost/fast_lms"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:rootroot@localhost/iitj-classroom"
+
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={}, future=True
